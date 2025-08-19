87b6833a058e2d93e06608a962e4c5f280eb1d4f
CdPilot1 <lachlan5890@gmail.com>
2025-08-19 14:07:41 +0930

    Added the contants, now prints resolution on startup

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import constants
 def main():
     print("Starting Asteroids!")
-
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
