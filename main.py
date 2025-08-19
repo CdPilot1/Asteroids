3020c1f8721f2a11d97805397d82a1b214eda18d
CdPilot1 <lachlan5890@gmail.com>
2025-08-20 00:32:07 +0930

    now it actually closes

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ if __name__ == "__main__":
     from constants import *
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
         screen.fill("black")
         pygame.display.flip()
