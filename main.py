437c2f538ac0f31850fe849620e85594ef41d686
CdPilot1 <lachlan5890@gmail.com>
2025-08-20 00:25:49 +0930

    added display, now shows a black screen

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+    pygame.init()
+    from constants import *
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
