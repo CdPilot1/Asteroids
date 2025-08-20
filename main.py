3530a39d72cbe616a07b0f7c870b21c9ca3d55e9
CdPilot1 <lachlan5890@gmail.com>
2025-08-20 11:39:38 +0930

    now added player sprite

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ if __name__ == "__main__":
     main()
     pygame.init()
     from constants import *
+    from player import *
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
